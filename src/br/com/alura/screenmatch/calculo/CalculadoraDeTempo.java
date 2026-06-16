807972e144520f61e17bc7b467a643b83574d48b
EricaFossatti <ericafossatti06@gmail.com>
2026-06-16 16:40:48 -0300

    Adiciona listas e coleções ao projeto

--- src/br/com/alura/screenmatch/calculo/CalculadoraDeTempo.java
+++ src/br/com/alura/screenmatch/calculo/CalculadoraDeTempo.java
@@ -1,11 +1,14 @@
 package br.com.alura.screenmatch.calculo;
 
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 import br.com.alura.screenmatch.modelos.Titulo;
 
 public class CalculadoraDeTempo {
     private int tempoTotal = 0;
 
     public int getTempoTotal() {
+
         return tempoTotal;
     }
 
--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -6,6 +6,10 @@ public class Filme extends Titulo implements Classificavel {
 //extends aplica a herança a uma classe
     private String diretor;
 
+    public Filme(String nome, int anoDeLancamento) {
+        super(nome, anoDeLancamento);
+    }
+
     public String getDiretor() {
         return diretor;
     }
@@ -17,5 +21,10 @@ public class Filme extends Titulo implements Classificavel {
     @Override
     public int getClassificacao() {
         return (int) pegaMedia() / 2;
+    }
+
+    @Override
+    public String toString() {
+        return "Filme: " + this.getNome() + " (" + this.getAnoDeLancamento() + ")";
     }
 }
